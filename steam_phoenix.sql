19a60edcbcfba7fa4097927f60846e38d891b1b4
jswift911 <jswift888@gmail.com>
2020-06-20 18:48:45 +0300

    Свежий дамп БД

--- steam_phoenix.sql
+++ steam_phoenix.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3307
--- Время создания: Май 17 2020 г., 18:44
+-- Время создания: Июн 20 2020 г., 18:47
 -- Версия сервера: 5.6.38-log
 -- Версия PHP: 7.1.12
 
@@ -44,8 +44,7 @@ CREATE TABLE `blogs` (
 INSERT INTO `blogs` (`id`, `title`, `text`, `img`, `created_at`, `updated_at`) VALUES
 (1, 'The best online game is out now!', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius-mod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Lorem ipsum dolor sit amet, consecte-tur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.....', 'assets/img/blog/1.jpg', '2020-05-13 12:02:20', '2020-05-13 12:02:20'),
 (2, 'The best online game is out now!', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius-mod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Lorem ipsum dolor sit amet, consecte-tur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.....', 'assets/img/blog/2.jpg', '2020-05-13 12:02:20', '2020-05-13 12:02:20'),
-(3, 'The best online game is out now!', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius-mod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Lorem ipsum dolor sit amet, consecte-tur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.....', 'assets/img/blog/3.jpg', '2020-05-13 12:03:12', '2020-05-13 12:03:12'),
-(4, 'The best online game is out now!', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius-mod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Lorem ipsum dolor sit amet, consecte-tur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.....', 'assets/img/blog/1.jpg', '2020-05-13 12:03:12', '2020-05-13 12:03:12');
+(3, 'The best online game is out now!', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eius-mod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Lorem ipsum dolor sit amet, consecte-tur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.....', 'assets/img/blog/3.jpg', '2020-05-13 12:03:12', '2020-05-13 12:03:12');
 
 -- --------------------------------------------------------
 
@@ -67,7 +66,7 @@ CREATE TABLE `features` (
 --
 
 INSERT INTO `features` (`id`, `title`, `text`, `img`, `created_at`, `updated_at`) VALUES
-(1, 'The game you’ve been waiting for is out now', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquamet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vestibulum posuere porttitor justo id pellentesque. Proin id lacus feugiat, posuere erat sit amet, commodo ipsum. Donec pellentesque vestibulum metus...', 'assets/img/featured-bg.jpg', '2020-05-13 12:16:31', '2020-05-13 12:16:31');
+(1, 'The game you’ve been waiting for is out now', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquamet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vestibulum posuere porttitor justo id pellentesque. Proin id lacus feugiat, posuere erat sit amet, commodo ipsum. Donec pellentesque vestibulum metus...', 'assets/img/featured-bg.jpg', '2020-05-13 12:16:31', '2020-06-10 09:23:18');
 
 -- --------------------------------------------------------
 
@@ -114,7 +113,8 @@ CREATE TABLE `intros` (
 INSERT INTO `intros` (`id`, `category`, `title`, `text`, `created_at`, `updated_at`) VALUES
 (1, 'Games', 'The best online game is out now!', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida....', '2020-05-13 11:52:50', '2020-05-13 11:52:50'),
 (2, 'Playstation', 'Top 5 best games in november', 'Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum labore suspendisse ultrices gravida....', '2020-05-13 11:55:15', '2020-05-13 11:55:15'),
-(3, 'Reviews', 'Get this game at a promo price', 'Sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida ncididunt ut labore....', '2020-05-13 11:55:48', '2020-05-13 11:55:48');
+(3, 'Reviews', 'Get this game at a promo price', 'Sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida ncididunt ut labore....', '2020-05-13 11:55:48', '2020-05-13 11:55:48'),
+(7, 'Reviews', 'Top 5 best games in november', 'Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum labore suspendisse ultrices gravida....', '2020-04-17 08:49:53', '2020-04-17 08:49:53');
 
 -- --------------------------------------------------------
 
@@ -136,11 +136,11 @@ CREATE TABLE `menus` (
 --
 
 INSERT INTO `menus` (`id`, `name`, `alias`, `created_at`, `updated_at`, `submenu`) VALUES
-(1, 'Home', 'home', '2020-05-13 09:44:55', '2020-05-13 09:44:55', NULL),
-(2, 'Games', 'games', '2020-05-13 09:45:28', '2020-05-13 09:45:28', 'Game Single'),
-(3, 'Reviews', 'reviews', '2020-05-13 09:45:49', '2020-05-13 09:45:49', NULL),
-(4, 'News', 'news', '2020-05-13 09:46:05', '2020-05-13 09:46:05', NULL),
-(5, 'Contacts', 'contacts', '2020-05-13 09:46:19', '2020-05-13 09:46:19', NULL);
+(1, 'Home', 'Home', '2020-05-13 09:44:55', '2020-05-26 12:06:24', NULL),
+(2, 'Games', 'Games', '2020-05-13 09:45:28', '2020-05-26 12:06:59', 'Game Single'),
+(3, 'Reviews', 'Reviews', '2020-05-13 09:45:49', '2020-05-26 12:06:34', NULL),
+(4, 'News', 'News', '2020-05-13 09:46:05', '2020-05-13 09:46:05', NULL),
+(5, 'Contacts', 'Contacts', '2020-05-13 09:46:19', '2020-06-20 09:01:17', NULL);
 
 -- --------------------------------------------------------
 
@@ -226,8 +226,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `avatar`, `steamid`, `role`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'jswift911', 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/7f/7f2d358ef0e79d35168979065e354d120f8ea769_full.jpg', '76561198129543963', 'admin', 'GCEKUIBpEs4KdshrDp06Egp1DQiJbN70gIPBmhIHaxVBVaSizU34Wmla8W8V', '2020-05-15 09:21:21', '2020-05-15 09:21:21'),
-(2, 'Toster', 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/c8/c82b74a992cae43b93ffca71d9688e60717d40d7_full.jpg', '76561198070025912', 'user', 'erFZnFU9vMbdxFmiTho0fp1JnwQYk0I1FMZbmZLjndpoOaXX0FgH0Fq2hvIz', '2020-05-15 10:03:42', '2020-05-15 10:34:25'),
+(1, 'jswift911', 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/7f/7f2d358ef0e79d35168979065e354d120f8ea769_full.jpg', '76561198129543963', 'admin', 'ud2WaY9WNfxkcQdsmL1qJYBbXLU1xwaPLWOd1shv4y7GJsXUCmpGHmyF0gdQ', '2020-05-15 09:21:21', '2020-05-15 09:21:21'),
+(2, 'Toster', 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/c8/c82b74a992cae43b93ffca71d9688e60717d40d7_full.jpg', '76561198070025912', 'user', 'kXSZUroOXMlYgogo6JLn4zyRyjgVOcnlMUsHPiOtai58qNgUUjyenrZykpWh', '2020-05-15 10:03:42', '2020-05-15 10:34:25'),
 (3, 'Moody', 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/f4/f42a606b37c2bfdf328b2b6cd3c65d90b723e925_full.jpg', '76561198056491553', 'user', NULL, '2020-05-17 15:34:27', '2020-05-17 15:34:27'),
 (4, 'BiG LEBOWSKi', 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/2e/2e8192df7f2de76163c3e597a20cb940ca87d7f7_full.jpg', '76561198041649056', 'user', NULL, '2020-05-17 15:38:40', '2020-05-17 15:38:40');
 
@@ -241,7 +241,7 @@ CREATE TABLE `videos` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `text` text COLLATE utf8_unicode_ci NOT NULL,
-  `promo_img` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `img` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `video_link` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -251,8 +251,8 @@ CREATE TABLE `videos` (
 -- Дамп данных таблицы `videos`
 --
 
-INSERT INTO `videos` (`id`, `title`, `text`, `promo_img`, `video_link`, `created_at`, `updated_at`) VALUES
-(1, 'Promo video of the game', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.', 'assets/img/promo-bg.jpg', 'https://www.youtube.com/watch?v=0sPbjHEnSDE', '2020-05-13 12:13:00', '2020-05-13 12:13:00');
+INSERT INTO `videos` (`id`, `title`, `text`, `img`, `video_link`, `created_at`, `updated_at`) VALUES
+(1, 'Promo video of the game', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.', 'assets/img/promo-bg.jpg', 'https://www.youtube.com/watch?v=0sPbjHEnSDE', '2020-05-13 12:13:00', '2020-06-20 06:34:01');
 
 --
 -- Индексы сохранённых таблиц
@@ -345,13 +345,13 @@ ALTER TABLE `filters`
 -- AUTO_INCREMENT для таблицы `intros`
 --
 ALTER TABLE `intros`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT для таблицы `menus`
 --
 ALTER TABLE `menus`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT для таблицы `migrations`
@@ -363,7 +363,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT для таблицы `sliders`
 --
 ALTER TABLE `sliders`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
